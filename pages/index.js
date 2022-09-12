ab74003f443a2f5c97eb7797c96bb370e46e4707
Hamzah Fansyuri <53176833+hf-lab@users.noreply.github.com>
2022-09-12 16:13:52 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
+        Learn English and IELTS Test at  <a href="https://www.ieltsclue.com/">IELTS Clue</a>
         </p>
       </main>
 
